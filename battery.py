caf0bb5847a114442a5d9a8f11d71fa3b7964084
Victor Emenike <emenikevn@yahoo.com>
2023-07-19 12:03:38 +0200

    modify service year for Spindler battery from 2 to 3

--- battery.py
+++ battery.py
@@ -36,7 +36,7 @@ class SpindlerBattery(BatteryType):
         self.current_date = current_date
 
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 3)
         return service_threshold_date < self.current_date
     
 
